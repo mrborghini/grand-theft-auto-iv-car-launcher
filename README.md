a36d96393b0246dc19e4a95817a5d5ddbf54a3a3
mrborghini <61702082+mrborghini@users.noreply.github.com>
2024-06-26 20:07:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Grand Theft Auto IV car launcher mod.
+
+This mod launches cars a random speed forward or backwards every 10 seconds.
+
+## How to install? (Make sure you have Grand Theft Auto IV version v1.0.7.0)
+
+1. [Download GTA 4 .Net ScriptHook](https://github.com/HazardX/gta4_scripthookdotnet/releases/tag/v1.7.1.7b)
+2. Unzip it and add add the files and folders to your GTA IV folder
+3. [Download the .dll file](/releases/) and put in the scripts folder of your GTA IV
+4. You should be able to launch GTA IV.
+
+## How to build the mod?
+1. Download and install [.NET Framework 4.8.1 Developer Pack](https://dotnet.microsoft.com/en-us/download/dotnet-framework/net481)
+2. Download and install any [.NET version](https://dotnet.microsoft.com/en-us/download) (It works with .NET 8.0)
+3. Run the following command:
+```
+dotnet build
+```
+4. The `.dll` file should be inside of the `bin` folder
---
+++ src/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using GTA;
+
+namespace gta_4_mod
+{
+    public class Car : Script
+    {
+        private Random _random;
+        public Car()
+        {
+            _random = new Random();
+            // Initialize the timer to trigger every 10 seconds
+            Game.DisplayText("Car launcher activated");
+
+            Timer timer = new Timer(10000);
+            timer.Tick += OnTimerTick;
+            timer.Start();
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            Game.DisplayText("10 seconds have passed");
+            Vehicle[] vehicles = World.GetAllVehicles();
+
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                if (vehicles[i].isAlive && Player.isActive)
+                {
+                    int speed = _random.Next(-100, 100);
+                    vehicles[i].Speed = speed;
+                }
+            }
+        }
+    }
+}
